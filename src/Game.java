2c181dd6956c0ed6e54c877d7d4fda7b00460b33
Jarad Touchberry <jaradtouchberry@Jarads-MacBook-Pro.local>
2017-04-04 12:45:01 -0400

    done

---
+++ src/Game.java
@@ -0,0 +1,44 @@
+/**
+ * Created by jaradtouchberry on 4/3/17.
+ */
+import java.util.Scanner;
+
+public class Game {
+    private static Scanner scanner = new Scanner(System.in);
+    public static Player player = new Player();
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Welcome, traveller.");
+
+        player.chooseName();
+        player.chooseWeapon();
+        player.chooseLocation();
+
+        player.findItem("shield");
+        player.findItem("boots");
+        player.findItem("belt"); }
+
+    public static String nextLine() {
+        String line = scanner.nextLine();
+        while (line.startsWith("/")) {
+            switch (line) {
+                case "/inv":
+                    for (String item : player.items) {
+                        System.out.println(item);
+                    }
+                    break;
+                case "/exit":
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Command not found!");
+                    break;
+            }
+            line = scanner.nextLine();
+        }
+        return line;
+
+
+
+    }
+}
---
+++ src/Player.java
@@ -0,0 +1,55 @@
+import java.util.ArrayList;
+
+/**
+ * Created by jaradtouchberry on 4/3/17.
+ */
+
+public class Player {
+    String name;
+    String weapon;
+    String location;
+    ArrayList<String> items = new ArrayList<>();
+
+
+    public void chooseName() {
+
+        System.out.println("What is your name?");
+        name = Game.nextLine();
+        System.out.println("Welcome, " + name);
+    }
+
+    public void chooseWeapon() throws Exception {
+        System.out.println("Choose your weapon [sword/mace]");
+        weapon = Game.nextLine();
+
+        if (weapon.equalsIgnoreCase("sword")) {
+            System.out.println("A sword is a fine choice!");
+        } else if (weapon.equalsIgnoreCase("mace")) {
+            System.out.println("A mace is a fine choice!");
+        } else {
+            throw new Exception("Invalid weapon.");
+        }
+    }
+
+    public void chooseLocation() throws Exception {
+        System.out.println("Choose your location [forest/tunnel]");
+        location = Game.nextLine();
+
+        if (location.equalsIgnoreCase("forest")) {
+            System.out.println("Entering forest...");
+        } else if (location.equalsIgnoreCase("tunnel")) {
+            System.out.println("Entering tunnel...");
+        } else {
+            throw new Exception("Invalid location.");
+        }
+    }
+
+    public void findItem(String item) {
+        System.out.println("You found a " + item + "! Pick it up? [y/n]");
+        String answer = Game.nextLine();
+        if (answer.equalsIgnoreCase("y")) {
+            items.add(item);
+            System.out.println("You picked up an item!");
+        }
+    }
+}
